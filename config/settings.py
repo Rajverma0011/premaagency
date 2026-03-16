e8acf98947515cd47311d8840ec8a1885284e2d9
Raj Verma <rajverma131004@gmail.com>
2026-03-16 23:36:15 +0530

    build tailwind css

--- config/settings.py
+++ config/settings.py
@@ -135,4 +135,4 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-#NPM_BIN_PATH = r"C:\Program Files\nodejs\npm.cmd"
+NPM_BIN_PATH = r"C:\Program Files\nodejs\npm.cmd"
